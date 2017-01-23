e2a054f67c29be46c6cb70039b305e0da56f6fe6
Jeevan Thomas Koshy <jeevan.thomaskoshy@gmail.com>
2017-01-23 23:47:21 +0530

    Loaded each images

--- notmnist.py
+++ notmnist.py
@@ -9,7 +9,7 @@ from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
-
+import cv2
 
 
 url = 'http://commondatastorage.googleapis.com/books1000/'
@@ -78,12 +78,39 @@ folder_names=["A","B","C","D","E","F","G","H","I","J"]
 for folder in folder_names:
 	im_name=random.choice(os.listdir(dir_name+'/'+folder))
 	im_file=dir_name+'/'+folder+'/'+im_name
-	display(Image(filename=im_file))
+	img=cv2.imread(im_file)
+	cv2.imshow("Image",img)
+	cv2.waitKey(0)
 
-	
+cv2.destroyAllWindows()	
 
 image_size=28
 pixel_depth=255.0
+
+def load_letter(folder,min_num_images):
+	"""Load the data for a single letter label"""
+	image_files=os.listdir(folder)
+	dataset=np.ndarray(shape=len(image_files),image_size,image_size,dtype=np.float32)
+	print(folder)
+	num_images=0
+	for image in image_files:
+		image_file=os.path.join(folder,image)
+		try:
+			image_data=(ndimage.imread(image_file).astype(float)-pixel_depth/2)/pixel_depth
+			if image_data.shape !=(image_size,image_size):
+				raise Exception("Unexpected Image Shape: %s" str(image_data.shape))
+			dataset[num_images,:,:]=image_data
+			num_images+=1
+		except IOError as e:
+			print('Could not read:',image_file,':',e,'- it\'s ok, skipping.')
+		dataset=datasetp[0:num_images,:,:]
+		if num_images<min_num_images:
+			raise Exception('Many fewer images than expected: %d < %d '%(num_images,min_num_images))
+		print('Full dataset tensor: ',dataset.shape)
+		print('Mean: ',np.mean(dataset))
+		print('Standard deviation: ',np.std(dataset))
+		return	dataset
+
 
 	
 
