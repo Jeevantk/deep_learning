478ebc0fce8d148b50bb6187befcf2298908982a
Jeevan Thomas Koshy <jeevan.thomaskoshy@gmail.com>
2017-02-01 22:41:53 +0530

    Verified Randomising

--- notmnist.py
+++ notmnist.py
@@ -204,9 +204,17 @@ print ('Testing',test_dataset.shape,test_labels.shape)
 def randomize(dataset,labels):
 	permutation=np.random.permutation(labels.shape[0])
 	shuffled_dataset=dataset[permutation,:,:]
-	shuffled_labels=labels[permutation,;,:]
+	shuffled_labels=labels[permutation]
+	return shuffled_dataset,shuffled_labels
 
-train_dataset,train_labels=randomize(train_dataset,train_labels)
-test_dataset,test_labels=randomize(test_dataset,test_labels)
-valid_dataset,valid_labels=randomize(valid_dataset,valid_labels)
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
 
+length=train_dataset.shape[0]-1
+
+for i in range(5):
+	index=np.random.randint(length)
+	plt.imshow(train_dataset[index,:,:])
+	plt.show()
+	print("This picture should be ",train_labels[index])
