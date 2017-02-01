8f45213c7948e2d645a6a4af8335f4058c7fc6fc
Jeevan Thomas Koshy <jeevan.thomaskoshy@gmail.com>
2017-02-01 22:14:47 +0530

    Added Merge datasets

--- notmnist.py
+++ notmnist.py
@@ -165,19 +165,39 @@ def merge_datasets(pickle_files,train_size,valid_size=0):
 	tsize_per_class=train_size //num_classes
 
 	start_v,start_t=0,0
-	end_v,end_t=vsize_per_class,t_size_per_class
-	end_l=vsize_per_class+t_size_per_class
+	end_v,end_t=vsize_per_class,tsize_per_class
+	end_l=vsize_per_class+tsize_per_class
 	for label,pickle_file in enumerate(pickle_files):
 		try:
 			with open(pickle_file,'rb') as f:
-				letter_set=pickle.load(f):
+				letter_set=pickle.load(f)
 				np.random.shuffle(letter_set)
 				if valid_dataset is not None:
 					valid_letter=letter_set[:vsize_per_class,:,:]
-					valid_dataset[start_v,end_v,:,:] =valid_letter
+					valid_dataset[start_v:end_v, :, :] = valid_letter
 					valid_labels[start_v:end_v]=label
 					start_v+=vsize_per_class
 					end_v+=vsize_per_class
 
 				train_letter=letter_set[vsize_per_class:end_l,:,:]
-				
+				train_dataset[start_t:end_t,:,:]=train_letter
+				train_labels[start_t:end_t]=label
+				start_t+=tsize_per_class
+				end_t+=tsize_per_class
+		except Exception as e:
+			print('Unable to process data from',pickle_file,':',e)
+			raise
+	return valid_dataset,valid_labels,train_dataset,train_labels
+
+
+train_size=200000
+valid_size=10000
+test_size=10000
+
+valid_dataset,valid_labels,train_dataset,train_labels=merge_datasets(train_datasets,train_size,valid_size)
+_,_,test_dataset,test_labels=merge_datasets(test_datasets,test_size)
+
+print ('Training:',train_dataset.shape,train_labels.shape)
+print ('Validation:',valid_dataset.shape,valid_labels.shape)
+print ('Testing',test_dataset.shape,test_labels.shape)
+
