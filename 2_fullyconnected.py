91b36b7fdc2f4bf7abaa45c9285ffe70e569644f
Jeevantk <jeevan.thomaskoshy@gmail.com>
2017-02-10 21:35:13 +0530

    made my own implementation, changes need to be made based on the pickle file. need to test

--- 2_fullyconnected.py
+++ 2_fullyconnected.py
@@ -67,43 +67,63 @@ num_steps=801
 def accuracy(predictions,labels):
 	return (100.0*np.sum(np.argmax(predictions,1)==np.argmax(labels,1))/predictions.shape[0])
 
-with tf.Session(graph=graph) as session:
-	tf.global_variable_initializer.run().run()
-	print('Initilized')
+# with tf.Session(graph=graph) as session:
+# 	tf.global_variable_initializer.run()
+# 	print('Initilized')
 
-	for step in range(num_steps):
-		_,l,predictions=session.run([optimiser,loss,train_prediction])
+# 	for step in range(num_steps):
+# 		_,l,predictions=session.run([optimiser,loss,train_prediction])
 
-		if (step%100==0):
-			print("Loss at step %d : %f " %(step,l))
-			print("Training accuracy: %.1f%%" %accuracy(predictions,train_labels[train_subset,:]))
+# 		if (step%100==0):
+# 			print("Loss at step %d : %f " %(step,l))
+# 			print("Training accuracy: %.1f%%" %accuracy(predictions,train_labels[train_subset,:]))
 
-			print("Validation accuracy :%.1f%%" %accuracy([valid_prediction.eval(),valid_labels]))
-			print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+# 			print("Validation accuracy :%.1f%%" %accuracy([valid_prediction.eval(),valid_labels]))
+# 			print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
 
 
 batch_size=128
 
 graph=tf.graph
 
+# with graph.as_default():
+# 	tf_train_dataset=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
+# 	tf_train_labels=tf.placeholder(tf.float32,shape=(batch_size,num_labels))
+# 	tf_train_dataset=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
+# 	tf_test_dataset=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
+
+# 	weights=tf.Variable(tf.truncated_normal([image_size*image_size,num_labels]))
+# 	biases=tf.Variable(tf.zeros([num_labels]))
+
+# 	logits=tf.matmul(tf_train_dataset,weights) + biases
+
+# 	loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels,logits=logits))
+# 	optimiser=tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+# 	train_prediction=tf.nn.softmax(logits)
+# 	valid_prediction=tf.nn.softmax(tf.matmul(tf_valid_dataset,weights)+biases)
+
+# 	test_prediction=tf.nn.softmax(tf.matmul(tf_train_dataset,weights)+biases)
+
+w_h=tf.Variable(tf.random_normal([784,1024], stddev=0.01))
+w_o=tf.Variable(tf.random_normal([1024,10], stddev=0.01))
+
+# Have to design it as a with a model in tensorflow
+
+
 with graph.as_default():
 	tf_train_dataset=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
 	tf_train_labels=tf.placeholder(tf.float32,shape=(batch_size,num_labels))
 	tf_train_dataset=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
 	tf_test_dataset=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
-
-	weights=tf.Variable(tf.truncated_normal([image_size*image_size,num_labels]))
-	biases=tf.Variable(tf.zeros([num_labels]))
-
-	logits=tf.matmul(tf_train_dataset,weights) + biases
-
+	#weights=tf.Variable(tf.truncated_normal([image_size*image_size,num_labels]))
+	
+	hidden_layer = tf.nn.relu(tf.matmul(X, w_h))
+	logits=tf.matmul(hidden_layer,w_o)
 	loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels,logits=logits))
 	optimiser=tf.train.GradientDescentOptimizer(0.5).minimize(loss)
-
 	train_prediction=tf.nn.softmax(logits)
-	valid_prediction=tf.nn.softmax(tf.matmul(tf_valid_dataset,weights)+biases)
 
-	test_prediction=tf.nn.softmax(tf.matmul(tf_train_dataset,weights)+biases)
 
 
 num_steps=3001
